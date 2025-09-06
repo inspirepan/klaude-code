6676a86ec55e425d2ebd42b4f8532bd724d8f90d
inspirepan <narfnas@163.com>
2025-09-06 10:13:25 +0800

    feat(ui): update debug event

--- src/codex_mini/trace/log.py
+++ src/codex_mini/trace/log.py
@@ -5,10 +5,10 @@ log_console = Console()
 
 
 def log(*objects: str | tuple[str, str]):
-    log_console.print(*((Text(obj[0], style=obj[1]) if isinstance(obj, tuple) else obj) for obj in objects))
+    log_console.print(*((Text(obj[0], style=obj[1]) if isinstance(obj, tuple) else Text(obj)) for obj in objects))
 
 
 def log_debug(*objects: str | tuple[str, str], style: str = "blue"):
     log_console.print(
-        *((Text(obj[0], style=obj[1]) if isinstance(obj, tuple) else obj) for obj in objects), style=style
+        *((Text(obj[0], style=obj[1]) if isinstance(obj, tuple) else Text(obj)) for obj in objects), style=style
     )
