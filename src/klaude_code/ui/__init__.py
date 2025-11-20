b30b1b05c4882a75c2b50d6a6946a948f6964d85
panjixiang <panjixiang@bytedance.com>
2025-11-21 00:34:55 +0800

    refactor(ui): move ExecDisplay to base module

--- src/klaude_code/ui/__init__.py
+++ src/klaude_code/ui/__init__.py
@@ -3,6 +3,6 @@ from .base.input_abc import InputProviderABC
 from .repl.display import REPLDisplay
 from .repl.input import PromptToolkitInput
 from .rich_ext.debug_event_display import DebugEventDisplay
-from .rich_ext.exec_display import ExecDisplay
+from .base.exec_display import ExecDisplay
 
 __all__ = ["DisplayABC", "InputProviderABC", "REPLDisplay", "PromptToolkitInput", "DebugEventDisplay", "ExecDisplay"]
